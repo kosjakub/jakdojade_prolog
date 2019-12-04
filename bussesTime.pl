0b1cccfc61d0d00514852673877cce7f94dd2671
arturro96 <arturwolak22@gmail.com>
2019-12-04 21:30:49 +0100

    merged

--- bussesTime.pl
+++ bussesTime.pl
@@ -80,17 +80,89 @@
     adjacent_stops(z,u,1).
 
 
-    
 findAllStops(Line, ListOfStops):-
         findall(Stop,(stop(Stop,NewLine), member(Line, NewLine)), ListOfStops).
 
-merge_adjacent([],[]).
-merge_adjacent([_],[]) :- !.
-merge_adjacent([X,Y|ZS], [M|MS]) :- intersection(X,Y,M), merge_adjacent([Y|ZS], MS).
+test1(SameValue, SameValue, Reduced) :-
+    2=1.
+    
+        
+reduce_busses([H|T], Reduced) :-
+                reduce_busses1(H, T, [], Reduced1),
+                flatten([H|T], OldFlat),
+                flatten(Reduced1, NewFlat),
+                length(OldFlat, OldLength),
+                length(NewFlat, NewLength),
+                OldFlat = NewFlat,
+                display(Reduced1),
+                reverse(Reduced1, Reduced).
 
-merge_list(A, L):-
-        merge_adjacent(A, L).
+reduce_busses([H|T], Reduced) :-
+                reduce_busses1(H, T, [], [HR| TR]),
+                flatten([H|T], OldFlat),
+                flatten([HR| TR], NewFlat),
+                length(OldFlat, OldLength),
+                length(NewFlat, NewLength),
+                \+OldFlat = NewFlat,
+                reduce_busses1(HR, TR, [], Reduced1),
+                Reduced = Reduced1.
+                
+reduce_busses1(X, [Y|[]], ReducedTmp, Reduced1) :-
+                intersection(X, Y, Intersection),
+                \+length(Intersection, 0),
+                %append(ReducedTmp, Intersection, Tmp),
+                %append(Tmp, Intersection, Tmp2),
+                Reduced1 = [Intersection, Intersection| ReducedTmp].
+                
+reduce_busses1(X, [Y|[]], ReducedTmp, Reduced1) :-   
+                intersection(X, Y, Intersection),
+                length(Intersection, 0),
+                %append(ReducedTmp, X, Tmp),
+                %append(Tmp, Y, Tmp2),
+                Reduced1 = [Y, X| ReducedTmp].            
 
+                
+reduce_busses1(X, [Y|InterTail], ReducedTmp, Reduced1) :-
+                intersection(X, Y, Intersection),
+                \+length(Intersection, 0),
+
+                %append(ReducedTmp, Intersection, Tmp),
+                reduce_busses1(Intersection, InterTail, [Intersection|ReducedTmp], Reduced1). 
+
+reduce_busses1(X, [Y|InterTail], ReducedTmp, Reduced1) :-
+                intersection(X, Y, Intersection),
+                length(Intersection, 0),
+                %append(ReducedTmp, X, Tmp),
+                reduce_busses1(Y, InterTail, [X|ReducedTmp], Reduced1).
+
+longest_sequence1([X,Y|T], Longest) :-
+                longest_sequence([X,Y|T], [], LongestReturn),
+                %display(LongestReturn),
+                reverse(LongestReturn, Longest).
+        
+longest_sequence([X|[]], LongestTmp, LongestReturn) :-
+                flatten(LongestTmp, Flat),
+                display(LongestTmp),
+                length(Flat, F),
+                length(LongestTmp, L),
+                \+F = L, 
+                reduce_busses(LongestTmp, Reduced),
+                LongestReturn = Reduced.
+                
+longest_sequence([X|[]], LongestTmp, LongestReturn) :-
+                flatten(LongestTmp, Flat),
+                length(Flat, F),
+                length(LongestTmp, L),
+                F = L,
+                LongestReturn = LongestTmp.
+
+                
+longest_sequence([X,Y|T], LongestTmp, LongestReturn) :- 
+                intersection(X, Y, L),
+                \+length(L, 0),
+                longest_sequence([Y|T], [L|LongestTmp], LongestReturn).
+
+               
 merge_adjacent_flatten([], []).
 merge_adjacent_flatten([_], []) :- !.
 merge_adjacent_flatten([X,Y|ZS], [M|MS]) :- 
@@ -99,23 +171,24 @@ merge_adjacent_flatten([X,Y|ZS], [M|MS]) :-
     intersection(X1,Y1,M), 
     merge_adjacent_flatten([Y|ZS], MS).
 
-occurrences_of(List, X, Count) :- aggregate_all(count, member(X, List), Count).
-        
+occurrences_of(List, X, Count) :- aggregate_all(count, member(X, List), Count).  
+
+
 route(Stop1, Stop2, Route, Time, ListOfLines):-                              
                 route1( Stop1, Stop2, [], RouteReturn, 0, TimeReturn, [], ListOfLinesReturn),
                 reverse([Stop2|RouteReturn], Route),
-                reverse(ListOfLinesReturn, ListOfLines),
+                stop(Stop2, L),
+                longest_sequence1([L|ListOfLinesReturn], Longest),
+                ListOfLines = Longest,
                 merge_adjacent_flatten(ListOfLines, Merged),
                 occurrences_of(Merged, [], Penalties),
                 Time is TimeReturn + Penalties*3.
-                
+
 route1(Stop1, Stop2, TempRoute, Route, TempTime, Time, TempLines, ListOfLines):-                    
         adjacent(Stop1, Stop2, TempTime1),
         \+member(Stop1, TempRoute),
         Route = [Stop1|TempRoute],
-        stop(Stop1, L1),
-        stop(Stop2, L2),
-        merge_list([L1, L2], L),
+        stop(Stop1, L),
         ListOfLines = [L|TempLines],
         Time is TempTime1 + TempTime.
 
@@ -124,7 +197,5 @@ route1(Stop1, Stop2, TempRoute, Route, TempTime, Time, TempLines, ListOfLines):-
         Next \== Stop2,
         \+member(Stop1, TempRoute),
         TempTime2 is TempTime1 + TempTime,
-        stop(Stop1, L1),
-        stop(Next, L2),
-        merge_list([L1, L2], L),
+        stop(Stop1, L),
         route1(Next, Stop2, [Stop1|TempRoute], Route, TempTime2, Time, [L|TempLines], ListOfLines).
